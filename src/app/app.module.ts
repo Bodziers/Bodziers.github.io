326c119e47bc9795dcea5854197ef2f2f2099e7f
Bogdan Pyrc <pyrc.bogdan@gmail.com>
2022-02-19 23:34:11 +0100

    adding draft of sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
